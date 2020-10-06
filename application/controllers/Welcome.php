eff343e863bf37a782eceec865a1e17e7f1af08f
Toni <sahmir.toni66@gmail.com>
2020-10-06 21:29:04 +0700

    pelatihan sesi ke-2

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -21,5 +22,17 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
+	}
+	public function lihat_info()
+	{
+		echo "<head><title>PELATIHAN CODEIGNITER 2020</title></head>";
+		echo "<h1>INI ADALAH PELATIHAN CODEIGNITER 2020</h1>";
+		echo "<h3>Presented by Fasilkom Unsub</h3>";
+		echo "<h3>Ini adalah pelatihan codeigniter versi 3.1.11</h3>";
+		echo "Fasilkom UNSUB 2020";
+	}
+	public function lihat_biodata()
+	{
+		$this->load->view('biodata');
 	}
 }
---
+++ application/views/biodata.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Biodataku</title>
+</head>
+
+<body>
+  <h1>Toni Sahmir H</h1>
+  <h3>Fakultas Ilmu Komputer UNSUB 2020</h3>
+  <table>
+    <tr>
+      <td>Kelas</td>
+      <td>:</td>
+      <td>5RA</td>
+    </tr>
+    <tr>
+      <td>Tanggal Lahir</td>
+      <td>:</td>
+      <td>30 November 1999</td>
+    </tr>
+    <tr>
+      <td>Alamat</td>
+      <td>:</td>
+      <td>Dirumah</td>
+    </tr>
+    <tr>
+      <td>Telepon</td>
+      <td>:</td>
+      <td>082240396621</td>
+    </tr>
+  </table>
+</body>
+
+</html>
